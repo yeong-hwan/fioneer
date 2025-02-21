20a68cb4f72ce836882f3be9da2dc758ff958202
Yeonghwan Jang <70074446+yeong-hwan@users.noreply.github.com>
2025-02-21 15:40:58 +0900

    refactor(ninjas): remove useless api key import

--- fioneer/ninjas/ninjas_client.py
+++ fioneer/ninjas/ninjas_client.py
@@ -8,12 +8,9 @@ class NinjasClient:
     BASE_URL = "https://api.api-ninjas.com/v1"
 
     def __init__(self):
-        self.api_key = os.getenv("FINNHUB_API_KEY")
         self.ninja_api_key = os.getenv("NINJA_API_KEY")
-        if not self.api_key:
-            raise ValueError("Finnhub API Key가 설정되지 않았습니다. .env 파일을 확인하세요.")
         if not self.ninja_api_key:
-            raise ValueError("Ninja API Key가 설정되지 않았습니다. .env 파일을 확인하세요.")
+            raise ValueError("Ninja API Key is not set")
 
     def get_earnings_transcript(self, symbol: str, year: int, quarter: int):
         url = f"{self.BASE_URL}/earningstranscript"
